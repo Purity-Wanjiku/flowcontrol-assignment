3bd2038552d01089fc5f34f6dac70abd9d7284c1
Purityndirangu <ndiranguwanjikupurity@gmail.com>
2023-03-22 18:09:40 +0300

    made corrections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,36 +4,46 @@ fun main(){
     var result = nameCheck(arrayOf("people","elephant","fruit","catherine","egg"))
     println(result)
 
-    partyDrinks(23)
+    partyDrinks(6)
 
     numCheck()
 }
+//Create a function that prints out all the odd numbers between 1 and 100
 fun numOdd(){
-    var z = 0
     for (x in 1..100)
-        if (x % 2 != 0){
+        if (x%2 != 0){
             println(x)
         }
 }
+//Create a function that takes in an array of names and returns the number of
+//names longer than 5 characters
 fun nameCheck(names: Array<String>): Int {
     var z = 0
     for (x in names){
-//    names.forEach{ x ->
         if (x.length>5) {
             z++
         }
     }
     return z
 }
+//You are creating a robot to serve drinks at a party. The robot is required to
+//serve a glass of milk for guests under the age of 6. For guests under the age of
+//15 but over the age of 6, the robot serves a bottle of fanta orange. Everybody
+//else gets a bottle of coca cola. Create a function that will make these
+//decisions, printing out the name of the drink each person gets according to
+//their age.
 fun partyDrinks(age: Int){
     when (age){
-        in 0..6 -> println("milk")
-        in 7..15 -> println("fanta orange")
+        in 0..5 -> println("milk")
+        in 7..14 -> println("fanta orange")
         else -> println("coca cola")
 
     }
 }
-
+//Write a function that prints each number from 1 to 100 on a new line. For each
+//multiple of 3, print "Fizz" instead of the number. For each multiple of 5, print
+//"Buzz" instead of the number. For numbers which are multiples of both 3 and
+//5, print "FizzBuzz" instead of the number.
 fun numCheck(){
     for (x in 1..100)
         if (x % 3 == 0 && x % 5 == 0) {
